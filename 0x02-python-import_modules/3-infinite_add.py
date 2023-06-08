bfaab4a70ac78c727791aee877a4187c9ea17228
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-06-08 14:11:58 -0700

    3. Infinite addition v2

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,8 @@ if __name__ == "__main__":
     import sys
     
     argv = sys.argv[1:]
-    i = 0
     mysum = 0
     args = len(argv)
     for i in range(args):
         mysum += int(argv[i])
-    print(mysum)
+    print("{}".format(mysum))
