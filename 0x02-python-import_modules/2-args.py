e90880c143484e7ed90b5bbccb4925ca630e9214
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-06-08 13:42:24 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+import sys
+
+argv = sys.argv
+plur = "s"
+dot = ":"
+i = 1
+args = len(argv) - 1
+if args == 1:
+    plur = ""
+if args == 0:
+    dot = "."
+print("{} argument{}{}".format(str(args), plur, dot))
+if args:
+    while not(i == args + 1):
+        print("{}: {}".format(str(i), argv[i]))
+        i += 1
