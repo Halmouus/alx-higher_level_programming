a1b0cf279d8291fc0b697e2f0f2a06b76f997963
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-06-08 14:06:48 -0700

    3. Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    
+    argv = sys.argv[1:]
+    i = 0
+    mysum = 0
+    args = len(argv)
+    for i in range(args):
+        mysum += int(argv[i])
+    print(mysum)
