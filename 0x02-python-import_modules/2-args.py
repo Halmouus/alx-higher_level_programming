fd70f3177b8aa5fdaed4a300ba4c4aa5f39f79fc
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-06-08 13:57:42 -0700

    2-args.py - v3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import sys
-
-argv = sys.argv
-plur = "s"
-dot = ":"
-i = 1
-args = len(argv) - 1
-if args == 1:
-    plur = ""
-if args == 0:
-    dot = "."
-print("{} argument{}{}".format(str(args), plur, dot))
-if args:
-    while not(i == args + 1):
-        print("{}: {}".format(str(i), argv[i]))
-        i += 1
+    import sys
+    
+    argv = sys.argv
+    plur = "s"
+    dot = ":"
+    i = 1
+    args = len(argv) - 1
+    if args == 1:
+        plur = ""
+    if args == 0:
+        dot = "."
+    print("{} argument{}{}".format(str(args), plur, dot))
+    if args:
+        while not(i == args + 1):
+            print("{}: {}".format(str(i), argv[i]))
+            i += 1
