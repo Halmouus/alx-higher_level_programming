018a8774923d9a7e60aa3840f57438e446ba13f5
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-06-12 02:03:55 -0700

    9. Find the max - v3

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list is None or len(my_list) == 0:
-        return (None)
+        return None
     sorted_list = sorted(my_list, reverse = True)
-    return (sorted_list[0])
+    return sorted_list[0)
