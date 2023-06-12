0a935afec1d018df437b92c1979a9660388096d1
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-06-12 06:29:45 -0700

    pycodestyle fix

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,5 +2,5 @@
 def max_integer(my_list=[]):
     if my_list is None or len(my_list) == 0:
         return None
-    sorted_list = sorted(my_list, reverse = True)
+    sorted_list = sorted(my_list, reverse=True)
     return sorted_list[0]
