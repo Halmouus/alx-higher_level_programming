d467a885ac101b3807d76d0c4a3a49b670179232
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-06-14 16:09:13 -0700

    13. Weighted average!

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list or len(my_list) == 0:
+        return 0
+    sum_w = sum(weight for score, weight in my_list)
+    sum_s = sum(score * weight for score, weight in my_list)
+    return sum_s / sum_w if sum_w != 0 else 0
