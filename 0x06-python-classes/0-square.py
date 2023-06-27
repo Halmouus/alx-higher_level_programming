5a4a8c79460172a566da60378a7688a625b70355
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-06-27 08:14:14 -0700

    0. My first square adding documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""0. My first square
+
+This module creates an empty class Square that defines a square
+"""
 class Square:
     """empty class for the Square object"""
     pass
