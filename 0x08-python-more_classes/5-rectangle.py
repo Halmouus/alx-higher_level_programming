e1ee68dc65d99d30fcb92698cec445318eb9a9fa
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-07-05 10:08:48 -0700

    testing checker

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,10 +1,66 @@
 #!/usr/bin/python3
-"""0. Simple rectangle
-
-This module creates an empty class Rectangle that defines a rectangle:
-"""
+"""5. Detect instance deletion"""
 
 
 class Rectangle:
-    """empty class for the Rectangle object"""
-    pass
+    """class for the Rectangle object"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """returns the current rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """returns the current rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """string format of a rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return ''
+        return ("#" * self.__width + "\n") * (self.__height - 1)\
+            + ("#" * self.__width)
+
+    def __repr__(self):
+        """String representation of the rectangle for eval"""
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        """Prints a message when a rectangle is deleted"""
+        print("Bye rectangle... ")
+
+    @property
+    def height(self):
+        """property to retrieve the height"""
+        return self.__height
+
+    @property
+    def width(self):
+        """property to retrieve the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """property to set the width"""
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+        else:
+            raise TypeError("width must be an integer")
+
+    @height.setter
+    def height(self, value):
+        """property to set the height"""
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
+        else:
+            raise TypeError("height must be an integer")
