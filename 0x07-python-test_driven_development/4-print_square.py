fb564bc4861f102244a24e21f7ad28c546d49280
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-07-04 19:51:24 -0700

    4. Text indentation

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""3. Print square
+
+ a function that prints a square with the character #.
+
+Prototype: def print_square(size):
+size is the size length of the square
+size must be an integer, otherwise raise a TypeError exception
+with the message size must be an integer
+if size is less than 0, raise a ValueError exception with the
+message size must be >= 0
+if size is a float and is less than 0, raise a TypeError exception
+with the message size must be an integer"""
+
+
+def print_square(size):
+    """
+    function name: print_square
+    arguments: size - integer
+    prints a square with '#'
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        for el in range(size):
+            print(size * '#')
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""4. Text indentation
+
+a function that prints a text with 2 new lines after
+each of these characters: ., ? and :
+
+Prototype: def text_indentation(text):
+text must be a string, otherwise raise a TypeError exception with
+the message text must be a string
+There should be no space at the beginning or at the end of
+each printed line"""
+
+
+def text_indentation(text):
+    """
+    function name: text_indentation
+    arguments: text - string
+    prints a text with 2 new lines after each of
+    these characters: ., ? and :
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    my_text = ""
+    delim = {'.', '?', ':'}
+    for char in text:
+        if char in delim:
+            my_text += char + "\n\n"
+        else:
+            my_text += char
+    lines = my_text.splitlines()
+    my_text = '\n'.join(line.lstrip() for line in lines)
+    if my_text and my_text[-1] == '\n':
+        my_text = my_text[:-1]
+    print(my_text)
