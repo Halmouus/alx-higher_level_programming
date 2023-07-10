7f576efc52fcc75bde522c69631e038686742e78
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-07-10 13:02:54 -0700

    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+"""Module that defines a class Square
+    that inherits from Rectangle"""
+
+
+class Square(Rectangle):
+    """class Square that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Instantiation with szie:
+        size must be private. No getter or setter
+        size must be a positive integer, validated
+        by integer_validator"""
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """returns area of a Square object"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """string format of a Rectangle object"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
