50e6f4b40625d74a37b196b410c6b2bf62bd0f22
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-07-10 10:41:04 -0700

    1. My list test checker

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Module that defines a class MyList
-that inherits from the object list"""
+"""
+MyList class module.
+
+Define MyList class.
+"""
 
 
 class MyList(list):
-    """a class MyList that inherits from list"""
+    """Define a MyList."""
+
     def print_sorted(self):
-        """prints the list, but sorted (ascending sort)"""
+        """Print the list sorted"""
         print(sorted(self))
