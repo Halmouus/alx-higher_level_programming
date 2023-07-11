ac9ec3a3a0958e840e458a0ace513d7ba1116a7f
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-07-11 13:22:32 -0700

    task 9

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,9 +5,9 @@ function that returns the dictionary description
 with simple data structure (list, dictionary, string,
 integer and boolean) for JSON serialization of an object"""
 
+
 def class_to_json(obj):
     """returns the dictionary descriptionwith simple data
     structure (list, dictionary, string,integer and boolean)
     for JSON serialization of an object"""
     return obj.__dict__
-
