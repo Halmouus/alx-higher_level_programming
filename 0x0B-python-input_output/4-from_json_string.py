92f3579649855d80e5ba986c1e93bbdaa28bacf7
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-07-11 12:45:39 -0700

    task 5

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""4. From JSON string to Object
+
+function that returns an object (Python data structure)
+represented by a JSON string"""
+import json
+
+
+def from_json_string(my_str):
+    """function that returns an object (Python data structure)
+    represented by a JSON string"
+    """
+    return json.loads(my_str)
