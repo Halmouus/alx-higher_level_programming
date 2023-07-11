eae48c6253b7772196ddf15cf90ee3805b0aa223
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-07-11 12:42:08 -0700

    task 4

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""2. Append to a file
+
+function that returns the JSON representation
+of an object (string)"""
+import json
+
+
+def to_json_string(my_obj):
+    """function that returns the JSON representation
+    of an object (string)
+    """
+    return json.dumps(my_obj)
