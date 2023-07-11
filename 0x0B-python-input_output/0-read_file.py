db9ba6e034de6cc308361e0cb70424c780a8de2f
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-07-11 12:23:16 -0700

    task 1 - fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     """function that reads a text file (UTF8)
     and prints it to stdout:
     """
-    with open(filename, encoding="UTF-8") as f:
-        print(f.read())
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end="")
