973c0ccb274b01fe5ada3b93f426dbb89f39de84
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-07-12 09:58:36 -0700

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""12. Pascal's Triangle
+
+function that that returns a list of lists of integers
+representing the Pascal’s triangle of n"""
+
+
+def pascal_triangle(n):
+    """function that that returns a list of lists of integers
+    representing the Pascal’s triangle of n
+    """
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                calc = triangle[i-1][j-1] + triangle[i-1][j]
+                row.append(calc)
+        triangle.append(row)
+    return triangle
