8632c69dc9a6f7d8d9aef8af25655a21043b50f3
Halmouus <javierminimaz@gmail.com>
2023-07-17 19:38:32 +0100

    pycodestyle

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""Unittest for Base class
+"""
+import unittest
+from models.base import Base
+
+
+class BaseTestCase(unittest.TestCase):
+    """Test class for Base class"""
+    def test_a_id(self):
+        """Test id coherence"""
+        self.assertEqual(Base().id, 1)
+        self.assertEqual(Base(None).id, 2)
+        self.assertEqual(Base().id, 3)
+        self.assertEqual(Base(12).id, 12)
+        self.assertEqual(Base().id, 4)
+
+    def test_b_duplication(self):
+        """Test id coherence and duplication"""
+        with self.assertRaises(ValueError):
+            Base(3)
+        with self.assertRaises(ValueError):
+            Base(1)
+        with self.assertRaises(ValueError):
+            Base(12)
+        self.assertEqual(Base(5).id, 5)
+        self.assertEqual(Base().id, 6)
+        with self.assertRaises(ValueError):
+            Base(6)
+        self.assertEqual(Base(5000).id, 5000)
+        with self.assertRaises(ValueError):
+            Base(3)
+        with self.assertRaises(ValueError):
+            Base(5000)
+
+    def test_c_special_values(self):
+        """Test for special values output"""
+        self.assertEqual(Base().id, 7)
+        self.assertEqual(Base(0).id, 0)
+        self.assertEqual(Base(-1).id, -1)
+        with self.assertRaises(ValueError):
+            Base(-1)
+        self.assertEqual(Base().id, 8)
+
+    def test_d_special_values_2(self):
+        """Test for special values output"""
+        self.assertEqual(Base().id, 9)
+        self.assertEqual(Base("str").id, 'str')
+        with self.assertRaises(ValueError):
+            Base("str")
+        self.assertEqual(Base().id, 10)
+
+    def test_e_class_type(self):
+        """Test for type and dictionary representation"""
+        self.assertEqual(str(type(Base())), "<class 'models.base.Base'>")
+        self.assertEqual(Base().__dict__, {'id': 13})
+
+    def test_f_special_values(self):
+        """Test for special values output : lists and tuples"""
+        self.assertEqual(Base([10, 20, 30]).id, [10, 20, 30])
+        with self.assertRaises(ValueError):
+            Base([10, 20, 30])
+        self.assertEqual(Base(['a', 'b', 'c']).id, ['a', 'b', 'c'])
+        with self.assertRaises(ValueError):
+            Base(['a', 'b', 'c'])
+        bs = Base('a', 1, 'str', 0, [0, 1, 2])
+        self.assertEqual(bs.id, ('a', 1, 'str', 0, [0, 1, 2]))
+        with self.assertRaises(ValueError):
+            Base(('a', 1, 'str', 0, [0, 1, 2]))
+
+    def test_g_private_attribute_access(self):
+        """Test for type and dictionary representation"""
+        with self.assertRaises(AttributeError):
+            Base().__nb_objects
+        with self.assertRaises(AttributeError):
+            Base().__id_list
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,182 @@
+#!/usr/bin/python3
+"""Unittest for Rectangle class
+"""
+import unittest
+from models.rectangle import Rectangle
+from models.base import Base
+from unittest.mock import patch
+import io
+
+
+class RectangleTestCase(unittest.TestCase):
+    """Test class for Rectangle class"""
+    def test_a_positional_arguments(self):
+        """Test positional argument validation"""
+        with self.assertRaises(TypeError):
+            Rectangle()
+        with self.assertRaises(TypeError):
+            Rectangle(1)
+
+    def test_a_the_ids(self):
+        """Test id coherence and duplication
+        and the validity of types and values"""
+        self.assertEqual(Rectangle(10, 9).id, 1)
+        self.assertEqual(Rectangle(10, 9, 0, 0, None).id, 2)
+        self.assertEqual(Rectangle(10, 9).id, 3)
+        self.assertEqual(Rectangle(10, 9, 0, 0, 12).id, 12)
+        self.assertEqual(Rectangle(10, 9, 0, 0).id, 4)
+        with self.assertRaises(ValueError):
+            Rectangle(10, 9, 0, 0, 3)
+        with self.assertRaises(ValueError):
+            Rectangle(10, 9, 0, 0, 1)
+        with self.assertRaises(ValueError):
+            Rectangle(10, 9, 0, 0, 12)
+        self.assertEqual(Rectangle(10, 9, 0, 0, 5).id, 5)
+        self.assertEqual(Rectangle(10, 9, 0, 0).id, 6)
+        with self.assertRaises(ValueError):
+            Rectangle(10, 9, 0, 0, 6)
+        self.assertEqual(Rectangle(10, 9, 0, 0, 5000).id, 5000)
+        with self.assertRaises(ValueError):
+            Rectangle(10, 9, 0, 0, 3)
+        with self.assertRaises(ValueError):
+            Rectangle(10, 9, 0, 0, 5000)
+
+    def test_b_width(self):
+        """Test width value validation"""
+        with self.assertRaises(TypeError):
+            Rectangle('str', 9, 0, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(0, 9, 0, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(-4, 9, 0, 0)
+        self.assertEqual(Rectangle(10, 9, 0, 0).id, 7)
+
+    def test_c_height(self):
+        """Test height value validation"""
+        with self.assertRaises(TypeError):
+            Rectangle(5, 'str', 0, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(5, 0, 0, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(5, -5, 0, 0)
+        self.assertEqual(Rectangle(10, 9, 0, 0).id, 8)
+
+    def test_d_x(self):
+        """Test x value validation"""
+        with self.assertRaises(TypeError):
+            Rectangle(5, 10, 'str', 0)
+        with self.assertRaises(ValueError):
+            Rectangle(5, 10, -1, 0)
+        self.assertEqual(Rectangle(5, 10, 4, 0).id, 9)
+
+    def test_e_y(self):
+        """Test y value validation"""
+        with self.assertRaises(TypeError):
+            Rectangle(5, 10, 0, 'str')
+        with self.assertRaises(ValueError):
+            Rectangle(5, 10, 0, -1)
+        self.assertEqual(Rectangle(5, 10, 0, 4).id, 10)
+
+    def test_f_type(self):
+        """Test type of Rectangle"""
+        class_type = "<class 'models.rectangle.Rectangle'>"
+        self.assertEqual(str(type(Rectangle(10, 5))), class_type)
+        self.assertEqual(Rectangle(10, 5).__dict__,
+                         {'_Rectangle__width': 10, '_Rectangle__height': 5,
+                         '_Rectangle__x': 0, '_Rectangle__y': 0, 'id': 13})
+
+    def test_g_type(self):
+        "Test type and isinstance"
+        self.assertTrue(type(Rectangle(10, 5)) == Rectangle)
+        self.assertFalse(type(Rectangle(10, 5)) == Base)
+        self.assertTrue(isinstance(Rectangle(10, 5), Rectangle))
+        self.assertTrue(isinstance(Rectangle(10, 5), Base))
+
+    def test_h_area(self):
+        """Test area public method of Rectangle"""
+        self.assertEqual(Rectangle(3, 2).area(), 6)
+        self.assertEqual(Rectangle(2, 10).area(), 20)
+        self.assertEqual(Rectangle(10, 5, 0, 0).area(), 50)
+        self.assertEqual(Rectangle(100, 50).area(), 5000)
+        with self.assertRaises(TypeError):
+            Rectangle(10, 5).area(3)
+
+    def test_i_display(self):
+        """Test display method of Rectangle"""
+        with patch('sys.stdout', new=io.StringIO()) as my_stdout:
+            Rectangle(5, 3).display()
+            disp = my_stdout.getvalue()
+        expected_disp = '#####\n#####\n#####\n'
+        self.assertEqual(disp, expected_disp)
+        with patch('sys.stdout', new=io.StringIO()) as my_stdout:
+            Rectangle(5, 3, 1, 0).display()
+            disp = my_stdout.getvalue()
+        expected_disp = ' #####\n #####\n #####\n'
+        self.assertEqual(disp, expected_disp)
+        with patch('sys.stdout', new=io.StringIO()) as my_stdout:
+            Rectangle(1, 1, 0, 1).display()
+            disp = my_stdout.getvalue()
+        expected_disp = '\n#\n'
+        self.assertEqual(disp, expected_disp)
+        with patch('sys.stdout', new=io.StringIO()) as my_stdout:
+            Rectangle(4, 4, 3, 2).display()
+            disp = my_stdout.getvalue()
+        expected_disp = '\n\n   ####\n   ####\n   ####\n   ####\n'
+        self.assertEqual(disp, expected_disp)
+        with self.assertRaises(TypeError):
+            Rectangle(10, 5, 1, 5).display(3)
+
+    def test_j_set_get_width(self):
+        "test width setters and getters"
+        rect = Rectangle(4, 2)
+        self.assertEqual(rect.width, 4)
+        rect.width = 10
+        self.assertEqual(rect.width, 10)
+        with self.assertRaises(TypeError):
+            rect.width = '6'
+        with self.assertRaises(ValueError):
+            rect.width = 0
+
+    def test_k_set_get_height(self):
+        "test height setters and getters"
+        rect = Rectangle(4, 2)
+        self.assertEqual(rect.height, 2)
+        rect.height = 10
+        self.assertEqual(rect.height, 10)
+        with self.assertRaises(TypeError):
+            rect.height = '6'
+        with self.assertRaises(ValueError):
+            rect.height = 0
+
+    def test_k_set_get_x(self):
+        "test x setters and getters"
+        rect = Rectangle(4, 2, 0, 0)
+        self.assertEqual(rect.x, 0)
+        rect.x = 10
+        self.assertEqual(rect.x, 10)
+        with self.assertRaises(TypeError):
+            rect.x = '12'
+        with self.assertRaises(ValueError):
+            rect.x = -3
+
+    def test_l_set_get_y(self):
+        "test y setters and getters"
+        rect = Rectangle(4, 2, 0, 0)
+        self.assertEqual(rect.y, 0)
+        rect.y = 10
+        self.assertEqual(rect.y, 10)
+        with self.assertRaises(TypeError):
+            rect.y = '12'
+        with self.assertRaises(ValueError):
+            rect.y = -3
+
+    def test_m_str(self):
+        "test string representation of Rectangle object"
+        r1 = Rectangle(4, 6, 2, 1, 100)
+        self.assertEqual(str(r1), "[Rectangle] (100) 2/1 - 4/6")
+        r2 = Rectangle(5, 10)
+        self.assertEqual(str(r2), "[Rectangle] (32) 0/0 - 5/10")
+
+
+if __name__ == '__main__':
+    unittest.main()
