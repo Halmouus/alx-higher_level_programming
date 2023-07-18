e932adebbc95842113c9fac6b84c103a2dec2ba2
Halmouus <javierminimaz@gmail.com>
2023-07-18 19:11:28 +0100

    pycodestyle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,7 +31,7 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
-    
+
     @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation json_string"""
@@ -45,10 +45,10 @@ class Base:
         jsonfile = cls.__name__ + ".json"
         if list_objs is None:
             list_objs = []
-        jsonstr = cls.to_json_string([inst.to_dictionary() for inst in list_objs])
+        jsonstr = cls.to_json_string([inst.to_dictionary()
+                                      for inst in list_objs])
         with open(jsonfile, "w") as f:
             f.write(jsonstr)
-
 
     @classmethod
     def id_list(cls):
