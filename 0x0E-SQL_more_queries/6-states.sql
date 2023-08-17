5a366502661251c1c6afe621b06ad4fa6ce4025a
Halmouus <javierminimaz@gmail.com>
2023-08-17 01:49:05 +0100

    adding unique

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states`(`id` PRIMARY KEY AUTO_INCREMENT NOT NULL,
-`name` VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states`(`id` PRIMARY KEY AUTO_INCREMENT NOT 
+UNIQUE, `name` VARCHAR(256) NOT NULL);
