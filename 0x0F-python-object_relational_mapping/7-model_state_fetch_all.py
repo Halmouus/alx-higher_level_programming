a9a2401cc4fc8a50a9999b2e1efd15f3d0d1fe93
Halmouus <javierminimaz@gmail.com>
2023-09-18 23:49:35 +0100

    task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""script that lists all State objects from the database"""
+
+import sys
+from sqlalchemy import create_engine, select
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ), pool_pre_ping=True,)
+
+    with engine.connect() as connection:
+        result = connection.execute(select(State).order_by(State.id.asc()))
+        for el in result:
+            print(f"{el.id}: {el.name}")
+
+    engine.dispose()
