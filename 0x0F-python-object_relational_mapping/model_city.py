f7148a7a8ea3e843231b5b9c0e2d5f938e84b08f
Halmouus <javierminimaz@gmail.com>
2023-09-23 01:41:58 +0100

    task 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""class definition of a City and an instance
+Base = declarative_base()"""
+
+import sqlalchemy
+from model_state import Base
+
+
+class City(Base):
+    """City class"""
+
+    __tablename__ = 'cities'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True,
+                           autoincrement=True, nullable=False)
+    name = sqlalchemy.Column(sqlalchemy.String(128), nullable=False)
+    state_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                 sqlalchemy.ForeignKey('states.id'))
