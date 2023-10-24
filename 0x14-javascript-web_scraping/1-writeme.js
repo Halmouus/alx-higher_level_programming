c513604ee88eac31800f246b8e4387e862c31758
Halmouus <javierminimaz@gmail.com>
2023-10-24 23:22:04 +0100

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const args = process.argv;
+const fs = require('fs');
+const filePath = args[2];
+const content = args[3];
+
+fs.writeFile(filePath, content, (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
