5ccc6ee6ba1fa3b7870400e9e933905165f0a938
Halmouus <javierminimaz@gmail.com>
2023-10-25 00:59:04 +0100

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const film of films) {
+      for (const character of film.characters) {
+        if (character.endsWith('/18/')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  } else {
+    console.error(error);
+  }
+});
