d059054c4c6f3f1d87382b8aeaa37e92586e5c1a
Halmouus <javierminimaz@gmail.com>
2023-10-24 23:35:05 +0100

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode) {
+    console.log(`code: ${response.statusCode}`);
+  } else {
+    console.error(error);
+  }
+});
