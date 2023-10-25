d109825d0836f90b818491c8a75449372c2475e9
Halmouus <javierminimaz@gmail.com>
2023-10-25 01:30:37 +0100

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], { json: true }, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const done = {};
+    for (const task of body) {
+      if (task.completed) {
+        done[task.userId] = (done[task.userId] || 0) + 1;
+      }
+    }
+    console.log(done);
+  } else {
+    console.error(error);
+  }
+});
