041725ba761f4c9a8b955974d05b6226f6bc8b99
Halmouus <javierminimaz@gmail.com>
2023-10-25 01:11:53 +0100

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(filePath, body, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+    // console.log(text);
+  } else {
+    console.error(error);
+  }
+});
